44a53ccc8fccba0692b46ea73db8fedca31b839f
kimmoperala <kimmoperala@gmail.com>
2020-03-27 10:51:08 +0200

    Small fix

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -5,9 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		ClockTimer timer = new ClockTimer();
 		DigitalClock clock = new DigitalClock(timer);
-		
-		timer.addObserver(clock);
-		
+				
 		new Thread(timer).start();
 	}
 
