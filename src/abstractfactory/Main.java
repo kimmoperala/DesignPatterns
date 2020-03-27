8175c2aac71e1e2ee99e9dcfe7da3233b3740d2e
kimmoperala <kimmoperala@gmail.com>
2020-03-27 11:46:27 +0200

    Assignment 2 bonus done

--- src/abstractfactory/Main.java
+++ src/abstractfactory/Main.java
@@ -1,20 +1,46 @@
 package abstractfactory;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Tehdas adidas = new TehdasAdidas();
-		Tehdas boss = new TehdasBoss();
+		Tehdas tehdas = null;
+		Class c = null;
 		Jasper jasper = new Jasper();
 		
+		Properties properties = new Properties();
+		
+		try {
+			properties.load(new FileInputStream("resources/tehdas.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			c = Class.forName(properties.getProperty("tehdas1"));
+			tehdas = (Tehdas) c.newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		System.out.println("Opiskelijana Jasperilla: ");
-		jasper.setTehdas(adidas);
+		jasper.setTehdas(tehdas);
 		jasper.pue();
 		System.out.println();
 		jasper.kerro();
 		
+		try {
+			c = Class.forName(properties.getProperty("tehdas2"));
+			tehdas = (Tehdas) c.newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		System.out.println("\n\nValmistuneena Jasperilla: ");
-		jasper.setTehdas(boss);
+		jasper.setTehdas(tehdas);
 		jasper.pue();
 		System.out.println();
 		jasper.kerro();
